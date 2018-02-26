e5caa1377cf86c8e3028d9a33f32edb9fb5b13b8
Vineet Reynolds Pereira <Vineet.Reynolds@gmail.com>
2018-02-27 04:42:24 +0530

    Added more tests

--- src/test/java/com/echidna/assessment/web/AuthorControllerTests.java
+++ src/test/java/com/echidna/assessment/web/AuthorControllerTests.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -45,5 +46,63 @@ public class AuthorControllerTests {
         .andExpect(status().isOk())
         .andExpect(jsonPath("$", hasSize(5)))
         .andExpect(jsonPath("$[0].name", is(authors.get(0).getName())));
+  }
+
+  @Test
+  public void givenAuthors_whenGetOneAuthor_thenReturnJsonObject() throws Exception {
+    List<Author> authors = Arrays.asList(new Author(1, "R.K. Narayan", "Author of Malgudi Days"),
+        new Author(2, "Rabindranath Tagore", "Author of Gitanjali"),
+        new Author(3, "Leo Tolstoy", "Russian writer"),
+        new Author(4, "Premchand", "Author in Hindi-Urdu"),
+        new Author(5, "Ruskin Bond", "Author in English"));
+
+    given(authorService.getAuthor(1)).willReturn(authors.get(0));
+    given(authorService.getAuthor(2)).willReturn(authors.get(1));
+    given(authorService.getAuthor(3)).willReturn(authors.get(2));
+    given(authorService.getAuthor(4)).willReturn(authors.get(3));
+    given(authorService.getAuthor(5)).willReturn(authors.get(4));
+
+    mvc.perform(get("/authors/1")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", is(notNullValue())))
+        .andExpect(jsonPath("$.name", is(authors.get(0).getName())));
+
+    mvc.perform(get("/authors/5")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", is(notNullValue())))
+        .andExpect(jsonPath("$.name", is(authors.get(4).getName())));
+  }
+
+  @Test
+  public void givenAuthors_whenGetMissingAuthor_thenReturnNotFound() throws Exception {
+    List<Author> authors = Arrays.asList(new Author(1, "R.K. Narayan", "Author of Malgudi Days"),
+        new Author(2, "Rabindranath Tagore", "Author of Gitanjali"),
+        new Author(3, "Leo Tolstoy", "Russian writer"),
+        new Author(4, "Premchand", "Author in Hindi-Urdu"),
+        new Author(5, "Ruskin Bond", "Author in English"));
+
+    given(authorService.getAuthor(1)).willReturn(authors.get(0));
+    given(authorService.getAuthor(2)).willReturn(authors.get(1));
+    given(authorService.getAuthor(3)).willReturn(authors.get(2));
+    given(authorService.getAuthor(4)).willReturn(authors.get(3));
+    given(authorService.getAuthor(5)).willReturn(authors.get(4));
+
+    mvc.perform(get("/authors/6")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().is4xxClientError());
+  }
+
+  @Test
+  public void givenNoAuthors_whenGetAuthors_thenReturnEmptyJsonArray() throws Exception {
+    List<Author> authors = Collections.emptyList();
+
+    given(authorService.listAll()).willReturn(authors);
+
+    mvc.perform(get("/authors")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(0)));
   }
 }
