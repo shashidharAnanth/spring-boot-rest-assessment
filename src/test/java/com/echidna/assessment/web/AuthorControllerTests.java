f2c746c02693801ad8161b1dde32096d22fb4906
Shashidhar <shashidhar.a@hebbalelabs.com>
2018-04-10 18:58:40 +0530

    Added testcases to validate name and description in request during POST /authors

--- src/test/java/com/echidna/assessment/web/AuthorControllerTests.java
+++ src/test/java/com/echidna/assessment/web/AuthorControllerTests.java
@@ -195,4 +195,54 @@ public class AuthorControllerTests {
         .andExpect(jsonPath("$.message", is("Validation failed")))
         .andExpect(jsonPath("$.details", is("Name should have min of 2 characters and max of 255")));
   }
+
+  @Test
+  public void givenAuthors_whenSaveAnAuthor_withMissingName_thenReturn4xxClientError() throws Exception {
+    List<Author> authors = Arrays.asList(new Author(1, "R.K. Narayan", "Author of Malgudi Days"),
+        new Author(2, "Rabindranath Tagore", "Author of Gitanjali"),
+        new Author(3, "Leo Tolstoy", "Russian writer"),
+        new Author(4, "Premchand", "Author in Hindi-Urdu"),
+        new Author(5, "Ruskin Bond", "Author in English"),
+        new Author(6, "", "Author in French"));
+
+    given(authorService.save(authors.get(0))).willReturn(authors.get(0));
+    given(authorService.save(authors.get(1))).willReturn(authors.get(1));
+    given(authorService.save(authors.get(2))).willReturn(authors.get(2));
+    given(authorService.save(authors.get(3))).willReturn(authors.get(3));
+    given(authorService.save(authors.get(4))).willReturn(authors.get(4));
+    given(authorService.save(authors.get(5))).willReturn(authors.get(5));
+
+    String author_5_json = mapper.writeValueAsString(authors.get(5));
+
+    mvc.perform(post("/authors")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(author_5_json))
+        .andExpect(status().is4xxClientError());
+  }
+
+  @Test
+  public void givenAuthors_whenSaveAnAuthor_withMissingDescription_thenReturn4xxClientError() throws Exception {
+    List<Author> authors = Arrays.asList(new Author(1, "R.K. Narayan", "Author of Malgudi Days"),
+        new Author(2, "Rabindranath Tagore", "Author of Gitanjali"),
+        new Author(3, "Leo Tolstoy", "Russian writer"),
+        new Author(4, "Premchand", "Author in Hindi-Urdu"),
+        new Author(5, "Ruskin Bond", "Author in English"),
+        new Author(6, "TM White", ""));
+
+    given(authorService.save(authors.get(0))).willReturn(authors.get(0));
+    given(authorService.save(authors.get(1))).willReturn(authors.get(1));
+    given(authorService.save(authors.get(2))).willReturn(authors.get(2));
+    given(authorService.save(authors.get(3))).willReturn(authors.get(3));
+    given(authorService.save(authors.get(4))).willReturn(authors.get(4));
+    given(authorService.save(authors.get(5))).willReturn(authors.get(5));
+
+    String author_5_json = mapper.writeValueAsString(authors.get(5));
+
+    mvc.perform(post("/authors")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(author_5_json))
+        .andExpect(status().is4xxClientError());
+  }
 }
